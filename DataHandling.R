438b67b0e64665a48835a290e7316e81dba725a4
KevinJoerg <kevin.joerg@gmail.com>
2019-12-10 19:35:58 +0100

    Update DataHandling.R
    Added 5 day returns

--- DataHandling.R
+++ DataHandling.R
@@ -26,15 +26,32 @@ for(s in allStocks){
   # Get the adjusted closing price with the adjustment factor
   priceAdjusted = t$prccd/t$ajexdi
   
-  # Get log returns
+  # Get returns
   logReturns = diff(log(priceAdjusted))
   simpleReturns = priceAdjusted[2:length(priceAdjusted)]/priceAdjusted[1:length(priceAdjusted)-1] - 1
+  
+  # Assign to dataframe
   df <- data.frame(simpleReturns,logReturns)
   colnames(df) <- c('simpleReturns','logReturns')
   rownames(df) <- t$datadate[2:length(priceAdjusted)]
   
-  # Create a dataframe with named after the stock
+  # Create a dataframe named after the stock
   assign(name,df)
+  
+  ## Same for 5 day returns
+  # Get returns
+  logReturns = diff(log(priceAdjusted),lag=5)
+  simpleReturns = priceAdjusted[6:length(priceAdjusted)]/priceAdjusted[1:(length(priceAdjusted)-5)] - 1
+  
+  # Assign to dataframe
+  df <- data.frame(simpleReturns,logReturns)
+  colnames(df) <- c('simpleReturns','logReturns')
+  rownames(df) <- t$datadate[6:length(priceAdjusted)]
+  
+  # Create a dataframe named after the stock
+  name <- paste(name,'5day',sep='')
+  assign(name,df)
+  
 }
 
 # Remove not needed variables
