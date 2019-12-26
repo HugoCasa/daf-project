92b255681eb92ba245e1d43e9bba35bb94700ec1
Hugo <hugo@casademont.ch>
2019-12-26 19:27:58 +0100

    Update model1.R

--- model1.R
+++ model1.R
@@ -7,7 +7,10 @@ library("MASS")
 ## Parameters
 
 # Portfolio directory in Data folder
-pf_path <- 'Data/Portfolio1/'
+# portfolio 1
+#pf_path <- 'Data/Portfolio1/'
+# portfolio 2
+pf_path <- 'Data/Portfolio2/'
 
 # Number of days ahead the VaR is calculated
 VaR_days <- 5
@@ -20,6 +23,14 @@ MC_n <- 1000
 
 # Conditional distribution GARCH: Students t distribution
 GARCHcondDist <- "std"
+
+# GARCH model
+#GARCH_model <- 'GARCH'
+GARCH_model <- 'TGARCH'
+
+# sample or fit t distribution on standardized returns
+#ret_method <- "sample"
+ret_method <- "fit"
 
 ## Run data handling file 
 source('DataHandling.R')
@@ -34,14 +45,28 @@ pf_log_mean <- mean(pf_log)
 pf_log_mean0 <- pf_log - pf_log_mean
 
 
-plot(pf_log)
+# fit garch model
 
-# garch
-#gfit01  <- garchFit(formula = ~ garch(1, 1), data=pf_log_mean0, cond.dist=GARCHcondDist)
-# tgarch
-gfit01 <- garchFit(formula = ~ garch(1, 1), delta =2, leverage = TRUE, data=pf_log_mean0, cond.dist=GARCHcondDist)
+if (GARCH_model == "GARCH") {
+  gfit01  <- garchFit(formula = ~ garch(1, 1), data=pf_log_mean0, cond.dist=GARCHcondDist)
+  
+  mu <- coef(gfit01)[["mu"]]
+  omega <- coef(gfit01)[["omega"]]
+  alpha <- coef(gfit01)[["alpha1"]]
+  beta <- coef(gfit01)[["beta1"]]
+}
+if (GARCH_model == "TGARCH") {
+  gfit01 <- garchFit(formula = ~ garch(1, 1), delta =2, leverage = TRUE, data=pf_log_mean0, cond.dist=GARCHcondDist)
+  
+  mu <- coef(gfit01)[["mu"]]
+  omega <- coef(gfit01)[["omega"]]
+  alpha <- coef(gfit01)[["alpha1"]]
+  beta <- coef(gfit01)[["beta1"]]
+  
+  gamma <- coef(gfit01)[["gamma1"]]
+}
 
-# Functions
+## Functions
 
 # GARCH
 
@@ -51,7 +76,6 @@ GARCH_ht_function <- function(omega,alpha,beta,hPrevious,zPrevious){
   return(h)
 }
 
-
 # TGARCH 
 
 TGARCH_ht_function <- function(omega,alpha,beta,gamma,hPrevious,zPrevious){
@@ -59,46 +83,52 @@ TGARCH_ht_function <- function(omega,alpha,beta,gamma,hPrevious,zPrevious){
   return(h)
 }
 
-
-mu <- coef(gfit01)["mu"]
-omega <- coef(gfit01)["omega"]
-alpha <- coef(gfit01)["alpha1"]
-beta <- coef(gfit01)["beta1"]
-
-#tgarch
-gamma <- coef(gfit01)["gamma1"]
-
+# conditional volatility
 h.t <- gfit01@h.t
+# standardized returns
 Z <- gfit01@residuals/gfit01@sigma.t 
 
-# fit student t dist
-Z_fit <- fitdistr(Z,"t")
-Z_mu <- Z_fit$estimate[["m"]]
-Z_s <- Z_fit$estimate[["s"]]
-Z_df <- Z_fit$estimate[["df"]]
-
-
-hist(Z,breaks=80,main='Standardised Residuals',freq=F,col='cyan')
-lines(seq(-10,10,0.01),dt((seq(-10,10,0.01)-Z_mu)/Z_s,Z_df)/Z_s,col='blue',lwd=2)
-# legend('topright',c('Fitted normal', 'Fitted t-distribution'),col=c('red', 'blue'),lwd=2)
-
-
-#MC_Z <- array(sample(Z,pf_days*VaR_days*MC_n,replace = TRUE), dim=c(pf_days,VaR_days,MC_n))
-MC_Z <- array(rt(pf_days*VaR_days*MC_n,df=Z_df), dim=c(pf_days,VaR_days,MC_n))
-MC_Z <- MC_Z * Z_s + Z_mu
-
-
-MC_h <- array(dim=c(pf_days,VaR_days,MC_n))
-
-MC_h[,1,] <- matrix(h.t,nrow=pf_days,ncol=MC_n)
-
-for(i in 2:VaR_days){
-  # garch
-  #MC_h[,i,] <- GARCH_ht_function(omega,alpha,beta,MC_h[,i-1,],MC_Z[,i-1,])
-  # tgarch
-   MC_h[,i,] <- TGARCH_ht_function(omega,alpha,beta,gamma,MC_h[,i-1,],MC_Z[,i-1,])
+if (ret_method == "sample") {
+  # sample from standardized residuals (3 dimensions: for each trading day, each nday VaR and each simulation per day)
+  MC_Z <- array(sample(Z,pf_days*VaR_days*MC_n,replace = TRUE), dim=c(pf_days,VaR_days,MC_n))
+}
+if (ret_method == "fit") {
+  
+  # fit student t distribution
+  Z_fit <- fitdistr(Z,"t")
+  Z_mu <- Z_fit$estimate[["m"]]
+  Z_s <- Z_fit$estimate[["s"]]
+  Z_df <- Z_fit$estimate[["df"]]
+  
+  # plot fitting
+  hist(Z,breaks=80,main='Standardised Residuals',freq=F,col='cyan')
+  lines(seq(-10,10,0.01),dt((seq(-10,10,0.01)-Z_mu)/Z_s,Z_df)/Z_s,col='red',lwd=2)
+  legend('bottomright',c('Fitted t-distribution'),col=c('red'), lwd=2)
+  
+  
+  # sample from fitted student t distribution (3 dimensions: for each trading day, each nday VaR and each simulation per day)
+  MC_Z <- array(rt(pf_days*VaR_days*MC_n,df=Z_df), dim=c(pf_days,VaR_days,MC_n))
+  # Scale back all standardized residuals
+  MC_Z <- MC_Z * Z_s + Z_mu
 }
 
+## Create array to store ht of simulations
+MC_h <- array(dim=c(pf_days,VaR_days,MC_n))
+
+# The variance of the first simulated day is the same for all
+MC_h[,1,] <- matrix(h.t,nrow=pf_days,ncol=MC_n)
+
+# Apply GARCH function
+for(i in 2:VaR_days){
+  if (GARCH_model == "GARCH") {
+    MC_h[,i,] <- GARCH_ht_function(omega,alpha,beta,MC_h[,i-1,],MC_Z[,i-1,])
+  }
+  if (GARCH_model == "TGARCH") {
+    MC_h[,i,] <- TGARCH_ht_function(omega,alpha,beta,gamma,MC_h[,i-1,],MC_Z[,i-1,])
+  }
+}
+
+## Simulate returns
 MC_log <- MC_Z*sqrt(MC_h)
 
 # Add back the mean to every stock
@@ -110,7 +140,6 @@ MC_log <- apply(MC_log,c(1,3),sum)
 
 # Apply quantile function to get VaR
 VaR <- apply(MC_log,1,quantile,VaR_alpha)
-
 
 
 ### Check model
