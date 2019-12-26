6a5dd5b045f14bd78166d11a519cf2263b44b1ad
KevinJoerg <kevin.joerg@gmail.com>
2019-12-26 19:28:29 +0100

    Create OutputFile.R

---
+++ OutputFile.R
@@ -0,0 +1,55 @@
+# The following code creates the output file
+
+
+write("=====================================================",outputFile_name)
+write("",outputFile_name,append=TRUE)
+write("Parameters:",outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+
+write(paste("Portfolio number:              ",pf_n),outputFile_name,append=TRUE)
+write(paste("VaR model:                     ",toString(2)),outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+
+write(paste("VaR Days:                      ",VaR_days),outputFile_name,append=TRUE)
+write(paste("VaR Alpha:                     ",VaR_alpha),outputFile_name,append=TRUE)
+write(paste("Number simulations:            ",MC_n),outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+
+write(paste("GARCH model:                   ",GARCH_model),outputFile_name,append=TRUE)
+write(paste("GARCH cond. dist:              ",GARCHcondDist),outputFile_name,append=TRUE)
+
+write("=====================================================",outputFile_name,append = TRUE)
+write("",outputFile_name,append=TRUE)
+write("Results:",outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+write(paste("Mean VaR:                      ",round(mean(VaR),digits = 4)),outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+write(paste("Exceedance ratio:              ",round(exRatio,digits = 4)),outputFile_name,append=TRUE)
+write(paste("Kupiec K:                      ",round(K,digits = 4)),outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+if(K < qchisq(p,1)){
+  write("VaR model is accurate at 99% level",outputFile_name,append=TRUE)
+}else{
+  write("VaR model is not accurate at 99% level",outputFile_name,append=TRUE)
+}
+
+write("=====================================================",outputFile_name,append = TRUE)
+write("",outputFile_name,append=TRUE)
+write("Unconditional summary statistics:",outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+write("Simulated nday portfolio returns:",outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+write(paste("Mean:                          ",round(MC_log_mean,digits = 4)),outputFile_name,append=TRUE)
+write(paste("Std:                           ",round(MC_log_std,digits = 4)),outputFile_name,append=TRUE)
+write(paste("Skewness                       ",round(MC_log_skewness,digits = 4)),outputFile_name,append=TRUE)
+write(paste("Kurtosis:                      ",round(MC_log_kurtosis,digits = 4)),outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+write("Observed nday portfolio returns:",outputFile_name,append=TRUE)
+write("",outputFile_name,append=TRUE)
+write(paste("Mean:                          ",round(mean(pf_log_nday),digits = 4)),outputFile_name,append=TRUE)
+write(paste("Std:                           ",round(sqrt(var(pf_log_nday)),digits = 4)),outputFile_name,append=TRUE)
+write(paste("Skewness                       ",round(skewness(pf_log_nday),digits = 4)),outputFile_name,append=TRUE)
+write(paste("Kurtosis:                      ",round(kurtosis(pf_log_nday,method = 'moment'),digits = 4)),outputFile_name,append=TRUE)
+
+write("=====================================================",outputFile_name,append = TRUE)
+write(paste("Time passed:                   ",round(difftime(time_end,time_start,units='mins'),2),' minutes'),outputFile_name,append=TRUE)
