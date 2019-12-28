2d7a5d10d1b95c5d43dd5afa7537599953f7497e
KevinJoerg <kevin.joerg@gmail.com>
2019-12-28 12:51:59 +0100

    Update main.R

--- main.R
+++ main.R
@@ -72,7 +72,9 @@ VaR2 <- VaR[1:length(time)]
 overall_time_end <- Sys.time()
 overall_time <- overall_time_end - overall_time_start
 
-# plotting
+## Comparison Plots
+
+# VaR and portfolio returns
 plot_name <- paste('plots/VaR_model1_vs_model2_pf',toString(pf_n),'_',toString(VaR_days),'day_',toString(VaR_alpha),'VaR_',MC_n,'simulations_',strftime(Sys.time(),format = "%Y-%m-%d--%H-%M-%S"),'.pdf',sep='')
 plot_main <- paste('Portfolio ',toString(pf_n),' - ',toString(VaR_days),' day ','- ',toString(VaR_alpha*100),'% VaR',sep='')
 plot_ylab <- paste(toString(VaR_days),'day log returns')
@@ -84,11 +86,18 @@ lines(index, VaR2, col="blue" )
 legend('topright',c('VaR Model 1', 'VaR Model 2'),col=c('red', 'blue'), lwd=2)
 dev.off()
 
+# Histogram VaR
+plot_name <- paste('plots/Histogram_VaR_model1_vs_model2_pf',toString(pf_n),'_',toString(VaR_days),'day_',toString(VaR_alpha),'VaR_',MC_n,'simulations_',strftime(Sys.time(),format = "%Y-%m-%d--%H-%M-%S"),'.pdf',sep='')
 h1 <- hist(-VaR1,breaks=30,freq=FALSE)
 h2 <- hist(-VaR2,breaks=30,freq=FALSE)
 plot_ymax <- max(max(unlist(h1['density'])),max(unlist(h2['density'])))
-plot(h1,col=rgb(1,0,0,1/2),ylim=c(0,plot_ymax),freq=FALSE)
-plot(h2,col=rgb(0,0,1,1/2),ylim=c(0,plot_ymax),freq=FALSE,add=TRUE)
+plot_xmin <- min(min(unlist(h1['breaks'])),min(unlist(h2['breaks'])))
+plot_xmax <- max(max(unlist(h1['breaks'])),max(unlist(h2['breaks'])))
+pdf(plot_name)
+plot(h1,col=rgb(1,0,0,1/2),ylim=c(0,plot_ymax),xlim=c(plot_xmin,plot_xmax),freq=FALSE,xlab='VaR',main='Histogram VaR')
+plot(h2,col=rgb(0,0,1,1/2),ylim=c(0,plot_ymax),xlim=c(plot_xmin,plot_xmax),freq=FALSE,add=TRUE)
+legend('topright',c('VaR Model 1', 'VaR Model 2'),col=c('red', 'blue'), lwd=2)
+dev.off()
 
 # Show Results
 printFile()
