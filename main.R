ff12e24fcad31876d9ba835451e975e15897ac86
KevinJoerg <kevin.joerg@gmail.com>
2020-01-04 14:03:08 +0100

    Update main.R

--- main.R
+++ main.R
@@ -15,7 +15,7 @@ library("psych")
 library("copula")
 
 # working directory
-setwd('/usr/R/daf-project')
+# setwd('/usr/R/daf-project')
 
 ### Parameters
 
@@ -23,13 +23,13 @@ setwd('/usr/R/daf-project')
 pf_n <- 2
 
 # Number of days ahead the VaR is calculated
-VaR_days <- 5
+VaR_days <- 10
 
 # VaR alpha
-VaR_alpha <- 0.05
+VaR_alpha <- 0.01
 
 # number of Monte Carlo simulations by day
-MC_n <- 5000
+MC_n <- 10000
 
 # Conditional distribution GARCH: Students t distribution
 GARCHcondDist <- "std"
@@ -41,6 +41,9 @@ GARCH_model <- 'TGARCH'
 # sample from observed returns or fit t distribution on standardized returns (for model 1)
 ret_method <- "sample"
 # ret_method <- "fit"
+
+# Memory of the pc in megabytes
+memory_mb <- 28000
 
 
 ### Files
